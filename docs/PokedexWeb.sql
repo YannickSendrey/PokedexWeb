40a4728262608b7f167c7a460c438c83667b7b59
YannickSendrey <yannicksendrey@gmail.com>
2023-09-12 13:05:07 +0200

    add script SQL
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

---
+++ docs/PokedexWeb.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "pokemon" (
+  "id" int PRIMARY KEY,
+  "name" varchar(255) NOT NULL,
+  "picture" varchar(255),
+  "hp" int,
+  "attack" int,
+  "defense" int,
+  "attackSpe" int,
+  "defenseSpe" int,
+  "speed" int,
+  "number" int
+);
+
+CREATE TABLE "type" (
+  "id" int PRIMARY KEY,
+  "name" varchar(255) NOT NULL
+);
+
+CREATE TABLE "user" (
+  "id" int PRIMARY KEY,
+  "username" varchar(255) NOT NULL,
+  "password" varchar(255) NOT NULL
+);
+
+CREATE TABLE "user_pokemon" (
+  "user_id" int NOT NULL,
+  "pokemon_id" int NOT NULL
+);
+
+CREATE TABLE "pokemon_type" (
+  "pokemon_id" int NOT NULL,
+  "type_id" int NOT NULL
+);
+
+ALTER TABLE "user_pokemon" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+
+ALTER TABLE "user_pokemon" ADD FOREIGN KEY ("pokemon_id") REFERENCES "pokemon" ("id");
+
+ALTER TABLE "pokemon_type" ADD FOREIGN KEY ("pokemon_id") REFERENCES "pokemon" ("id");
+
+ALTER TABLE "pokemon_type" ADD FOREIGN KEY ("type_id") REFERENCES "type" ("id");
