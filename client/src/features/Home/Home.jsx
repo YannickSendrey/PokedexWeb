8a9e1993fe4f739002fc1b2b9692a926066c09bf
YannickSendrey <yannicksendrey@gmail.com>
2023-09-23 18:19:06 +0200

    layout responsive ok
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

--- client/src/features/Home/Home.jsx
+++ client/src/features/Home/Home.jsx
@@ -4,17 +4,17 @@ import styles from '../../css/home.module.css';
 export const Home = () => {
 	return (
 		<main className={styles.main}>
-			<div className={styles.main_sign}>
-				<div className={styles.main_sign_div}>Sign-up</div>
-				<p className={styles.main_sign_text}>
-					Already Registered ? Click{' '}
-					<a className={styles.main_sign_link}>Here to sign-in !</a>
+			<div className={styles.main_div}>
+				<div className={styles.main_innerDiv}>Sign-up</div>
+				<p className={styles.main_text}>
+					Already Registered ? Click
+					<a className={styles.main_link}> Here to sign-in !</a>
 				</p>
 			</div>
 
-			<div className={styles.main_guest}>
-				<div className={styles.main_guest_div}>Access as a guest</div>
-				<p className={styles.main_guest_text}>
+			<div className={styles.main_div}>
+				<div className={styles.main_innerDiv}>Access as a guest</div>
+				<p className={styles.main_text}>
 					You won't have access to team-building feature...
 				</p>
 			</div>
