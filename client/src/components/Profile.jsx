8dd76759f79951d8d221444ff08e2c3096e24504
YannickSendrey <yannicksendrey@gmail.com>
2023-09-25 08:48:47 +0200

    redirect to sign-in if not logged in
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	loadFavoritePokemons,
+	selectFavoritePokemons,
+} from '../features/PokemonBoard/pokemonBoardSlice';
 
 export const Profile = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 	const username = localStorage.getItem('username');
 	const id = localStorage.getItem('userId');
+	const favoritePokemons = useSelector(selectFavoritePokemons);
+
+	useEffect(() => {
+		if (!localStorage.getItem('username')) {
+			navigate('/sign-in');
+		}
+		dispatch(loadFavoritePokemons(id));
+	}, []);
 
 	return (
 		<main>
